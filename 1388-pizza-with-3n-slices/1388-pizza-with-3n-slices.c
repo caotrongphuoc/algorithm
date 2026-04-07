f2c0a7544093aeeb12fce46bb0e15c1dde1259a4
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-04-07 11:06:48 +0700

    Difficulty: Hard

--- 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.c
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.c
@@ -1,3 +1,56 @@
-int maxSizeSlices(int* slices, int slicesSize) {
-    
+int max(int a, int b)
+{
+    return a > b ? a : b;
+}
+
+//solve tuyen tinh voi O(k) space
+int solve(int *arr, int n, int k)
+{
+    int *prev = (int*)calloc(k + 1, sizeof(int));
+    int *curr = (int*)calloc(k + 1, sizeof(int));
+    int *prev2 = (int*)calloc(k + 1, sizeof(int));
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 1; j <= k; j++)
+        {
+            //Ko chon
+            curr[j] = prev[j];
+
+            //Chon
+            int take = arr[i];
+            if(i >= 1)
+            {
+                take += prev2[j - 1];
+            }
+                curr[j] = max(curr[j], take);
+        }
+
+            //dich mang
+            int* temp = prev2;
+            prev2 = prev;
+            prev = curr;
+            curr = temp;
+    }
+
+    int res = prev[k];
+
+    free(prev);
+    free(curr);
+    free(prev2);
+
+    return res;
+}
+
+int maxSizeSlices(int* slices, int slicesSize) 
+{
+   int k = slicesSize / 3;
+
+   //case 1: bo cuoi
+   int res1 = solve(slices, slicesSize - 1, k);
+
+   //case 2: bo dau
+   int res2 = solve(slices + 1, slicesSize - 1, k);
+
+   return max(res1, res2);
 }
