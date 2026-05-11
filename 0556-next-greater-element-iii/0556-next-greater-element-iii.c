67103cb367f742d14f8a47a693e0749a5c0fb82c
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-05-11 23:49:44 +0700

    Time: 0 ms (100%), Space: 8.6 MB (7.69%) - LeetHub

---
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.c
@@ -0,0 +1,43 @@
+int nextGreaterElement(int n) {
+    char s[12];
+
+    sprintf(s, "%d", n);
+
+    int len = strlen(s);
+
+    int i = len - 2; //Vi tri so ben trai nho hon so ben phai
+
+    while(i >=0 && s[i] >= s[i + 1]) {
+        i--;
+    }
+
+    if(i < 0) return -1; //Ko co so ben trai nho hon so ben phai
+
+    int j = len - 1;
+
+    while(s[j] <= s[i]) j--;
+
+    int temp = s[i];
+    s[i] = s[j];
+    s[j] = temp;
+
+    //Dao nguoc de co so nho nhat co the
+    int l = i + 1;
+    int r = len - 1;
+
+    while(l < r) {
+        temp = s[l];
+        s[l] = s[r];
+        s[r] = temp;
+        l++;
+        r--;
+    }
+
+    long ans = atoll(s); //Doi lai thanh so;
+
+    if(ans > INT_MAX) {
+        return -1;
+    }
+
+    return (int)ans;
+}
